c015f8df6e5ede771ad555dc5b9ec1c9a22fb15c
logitron <mpatricks@gmail.com>
2019-05-23 09:36:58 -0400

    Linked NhdVideos page to YouTube channel

---
+++ app/pages/NhdVideos.vue
@@ -0,0 +1,27 @@
+<template>
+  <Page actionBarHidden="true">
+    <StackLayout>
+      <WebView src="https://www.youtube.com/user/nationalhistory" height="100%" />
+    </StackLayout>
+  </Page>
+</template>
+
+<script>
+  export default {
+    name: 'NhdVideos',
+    displayName: 'NHD Videos',
+    icon: 'fa-film',
+
+    data () {
+      return {
+      }
+    },
+
+    methods: {
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
