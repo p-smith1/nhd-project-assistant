c14328ea3c726a27d4e0f7050aae03c545b3f695
logitron <mpatricks@gmail.com>
2019-05-23 09:35:53 -0400

    Trimmed down to MVP pages

--- app/router/index.js
+++ app/router/index.js
@@ -1,23 +1,13 @@
 import Home from '~/pages/Home'
 import GettingStarted from '~/pages/GettingStarted'
-import HistoricalThinking from '~/pages/HistoricalThinking'
 import ResearchTools from '~/pages/ResearchTools'
-import ThesisStatements from '~/pages/ThesisStatements'
-import ProjectOrganization from '~/pages/ProjectOrganization'
-import ProcessPaper from '~/pages/ProcessPaper'
-import StudentSamples from '~/pages/StudentSamples'
-import OnlineClassroomVideos from '~/pages/OnlineClassroomVideos'
+import NhdVideos from '~/pages/NhdVideos'
 
 const routes = {
   Home,
   GettingStarted,
-  HistoricalThinking,
   ResearchTools,
-  ThesisStatements,
-  ProjectOrganization,
-  ProcessPaper,
-  StudentSamples,
-  OnlineClassroomVideos
+  NhdVideos
 }
 
 export default routes
