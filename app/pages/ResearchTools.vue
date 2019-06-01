4d035bfb45266d47e67aee2e0116751157adb53a
logitron <mpatricks@gmail.com>
2019-06-01 11:52:04 -0400

    Added content to Research Tools page

--- app/pages/ResearchTools.vue
+++ app/pages/ResearchTools.vue
@@ -1,35 +1,65 @@
 <template>
   <Page actionBarHidden="true">
-    <StackLayout>
-      <Image src="~/assets/images/galileo-logo.png" @tap="openGalileo" backgroundColor="blue" class="research-link" />
-
-      <Image src="~/assets/images/digi-lib-ga-logo.png" @tap="openDigitalLibraryGa" class="research-link" />
-    </StackLayout>
+    <ScrollView>
+      <ListView for="tool in researchTools" @itemTap="onResearchToolTap">
+        <v-template>
+          <Image :src="tool.imageSrc" width="80%" class="m-y-5" />
+        </v-template>
+      </ListView>
+    </ScrollView>
   </Page>
 </template>
 
 <script>
-  const utilsModule = require("tns-core-modules/utils/utils")
-  import DigitalLibraryGeorgia from '~/pages/DigitalLibraryGeorgia'
-  import Galileo from '~/pages/Galileo'
+  import * as utilsModule from 'tns-core-modules/utils/utils'
 
   export default {
     name: 'ResearchTools',
     displayName: 'Research Tools',
     icon: 'fa-wrench',
     data () {
-      return {};
+      return {
+        researchTools: [
+          {
+            name: 'ResearchTool_Galileo',
+            imageSrc: '~/assets/images/galileo-logo.png',
+            url: 'http://www.galileo.usg.edu/express?link=nhda'
+          },
+          {
+            name: 'ResearchTool_DigitalLibrarayGeorgia',
+            imageSrc: '~/assets/images/digi-lib-ga-logo.png',
+            url: 'https://dlg.usg.edu'
+          },
+          {
+            name: 'ResearchTool_NewGaEncyclopedia',
+            imageSrc: '~/assets/images/new-ga-encyclopedia-logo.png',
+            url: 'https://www.georgiaencyclopedia.org'
+          },
+          {
+            name: 'ResearchTool_GaArchives',
+            imageSrc: '~/assets/images/ga-archives-logo.png',
+            url: 'https://www.georgiaarchives.org'
+          },
+          {
+            name: 'ResearchTool_LewisLibraryLibGuides',
+            imageSrc: '~/assets/images/lagrange-college-logo.png',
+            url: 'https://lagrange.libguides.com/nhd'
+          },
+          {
+            name: 'ResearchTool_ChroniclingAmerica',
+            imageSrc: '~/assets/images/chronicling-america-logo.png',
+            url: 'https://chroniclingamerica.loc.gov'
+          }
+        ]
+      }
     },
 
     props: ['currentPage'],
 
     methods: {
-      openGalileo: function () {
-        this.$emit('update-current-page', Galileo)
-      },
-
-      openDigitalLibraryGa: function () {
-        this.$emit('update-current-page', DigitalLibraryGeorgia)
+      onResearchToolTap: function (e) {
+        this.$emit('update-current-page', e.item.name)
+        utilsModule.openUrl(e.item.url)
       }
     }
   }
