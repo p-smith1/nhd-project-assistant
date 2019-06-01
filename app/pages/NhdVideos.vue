070699e83121c2e4776f9941da5545c105b46f8b
logitron <mpatricks@gmail.com>
2019-06-01 12:45:32 -0400

    Added two YouTube channels to Nhd Videos Page

--- app/pages/NhdVideos.vue
+++ app/pages/NhdVideos.vue
@@ -3,10 +3,20 @@
     <StackLayout>
       <WebView src="https://www.youtube.com/user/nationalhistory" height="100%" />
     </StackLayout>
+
+    <StackLayout class="m-10" height="100%" verticalAlignment="middle">
+      <ListView for="channel in channels">
+        <v-template>
+          <Button :text="channel.text" class="m-y-10" @tap="onChannelTap(channel)" />
+        </v-template>
+      </ListView>
+    </StackLayout>
   </Page>
 </template>
 
 <script>
+  import * as utilsModule from 'tns-core-modules/utils/utils'
+
   export default {
     name: 'NhdVideos',
     displayName: 'NHD Videos',
@@ -14,10 +24,27 @@
 
     data () {
       return {
+        channels: [
+          {
+            name: 'NhdVideos_NhdYouTubeChannel',
+            url: 'https://www.youtube.com/user/nationalhistory',
+            text: 'National History Day (NHD) YouTube Channel'
+          },
+          {
+            name: 'NhdVideos_NhdGeorgiaYouTubeChannel',
+            url: 'https://www.youtube.com/channel/UClWdIiQNkw26Tz4YQ77Hi5A',
+            text: 'NHD Georgia YouTube Channel'
+          }
+        ]
       }
     },
 
     methods: {
+      onChannelTap: function (channel) {
+        this.$emit('update-current-page', channel.name)
+
+        utilsModule.openUrl(channel.url)
+      }
     }
   }
 </script>
