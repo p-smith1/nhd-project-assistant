7461ab0b4e1b38c59245d0080afc1f048b49786c
logitron <mpatricks@gmail.com>
2019-06-01 11:51:32 -0400

    Added Donate page

---
+++ app/pages/Donate.vue
@@ -0,0 +1,27 @@
+<template>
+  <Page actionBarHidden="true">
+    <StackLayout>
+      <WebView src="https://www.georgiahumanities.org/donate-form" height="100%" />
+    </StackLayout>
+  </Page>
+</template>
+
+<script>
+  export default {
+    name: 'Donate',
+    displayName: 'Donate',
+    icon: 'fa-gift',
+
+    data () {
+      return {
+      }
+    },
+
+    methods: {
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
