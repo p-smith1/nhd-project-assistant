ee5316708a89b37cbe40c1a1b841377e38c11546
logitron <mpatricks@gmail.com>
2019-06-01 11:19:18 -0400

    Added Donate to routes.

--- app/router/index.js
+++ app/router/index.js
@@ -2,12 +2,14 @@ import Home from '~/pages/Home'
 import GettingStarted from '~/pages/GettingStarted'
 import ResearchTools from '~/pages/ResearchTools'
 import NhdVideos from '~/pages/NhdVideos'
+import Donate from '~/pages/Donate'
 
 const routes = {
   Home,
   GettingStarted,
   ResearchTools,
-  NhdVideos
+  NhdVideos,
+  Donate
 }
 
 export default routes
