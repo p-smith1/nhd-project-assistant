ef5fcb61762fde1c0c1b8e66338679349bbb7375
logitron <mpatricks@gmail.com>
2019-06-01 11:19:41 -0400

    Improvements to Home Page * Added Upcoming Events * Added Sponsors

--- app/pages/Home.vue
+++ app/pages/Home.vue
@@ -1,8 +1,22 @@
 <template>
   <Page actionBarHidden="true">
-    <GridLayout columns="*" rows="*">
-      <Image src="~/assets/images/nhd-logo.jpg" width="50%" />
-    </GridLayout>
+    <StackLayout class="m-10">
+      <Image src="~/assets/images/nhd-ga-logo.png" width="50%" class="m-b-20" />
+
+      <Label text="Upcoming Events" class="font-weight-bold" />
+      <Label class="hr-dark m-10" />
+      <Label textWrap="true" text="The 2019 National Contest will be held June 9-13, 2019 at the University of Maryland, College Park." />
+      <Label class="hr-light m-10" />
+      <Label textWrap="true" text="The 2018-2019 theme is Triumph &amp; Tragedy in History" />
+      <Label class="hr-light m-10" />
+
+      <Label text="Sponsors" class="font-weight-bold m-t-30" />
+      <Label class="hr-dark m-10" />
+      <StackLayout orientation="horizontal">
+        <Image src="~/assets/images/ga-humanities-logo.png" width="50%" />
+        <Image src="~/assets/images/lagrange-college-logo.png" width="50%" />
+      </StackLayout>
+    </StackLayout>
   </Page>
 </template>
 
