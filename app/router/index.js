529a671698e15e7954e2fbc2ea7dd898b185cb4f
logitron <mpatricks@gmail.com>
2019-08-03 11:13:05 -0400

    Removed Donation Page & Route

--- app/router/index.js
+++ app/router/index.js
@@ -2,14 +2,12 @@ import Home from '~/pages/Home'
 import GettingStarted from '~/pages/GettingStarted'
 import ResearchTools from '~/pages/ResearchTools'
 import NhdVideos from '~/pages/NhdVideos'
-import Donate from '~/pages/Donate'
 
 const routes = {
   Home,
   GettingStarted,
   ResearchTools,
   NhdVideos,
-  Donate,
   ContactUs: {
     icon: 'fa-envelope',
     displayName: 'Contact Us',
