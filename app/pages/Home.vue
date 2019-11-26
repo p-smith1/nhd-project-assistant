ad51b3871d389ea6b616ee2f1d08f53dc5873edf
Patrick Smith <patrick.smith@kashew.net>
2019-11-26 08:03:29 -0500

    Removed Upcoming Events from Home Page

--- app/pages/Home.vue
+++ app/pages/Home.vue
@@ -5,13 +5,6 @@
         <Image src="~/assets/images/nhd-ga-logo.png" width="35%" />
         <Label textWrap="true" class="m-b-20" text="Welcome to NHD Georgia. Our goal is to help you get the most out of your History Day Project. You are joining a global community of NHD scholars totaling more than half a million last year. So, welcome to the adventure that is National History Day!" />
 
-        <Label text="Upcoming Events" class="font-weight-bold" />
-        <Label class="hr-dark m-10" />
-        <Label textWrap="true" text="The 2019 National Contest will be held June 9-13, 2019 at the University of Maryland, College Park." />
-        <Label class="hr-light m-10" />
-        <Label textWrap="true" text="The 2018-2019 theme is Triumph &amp; Tragedy in History" />
-        <Label class="hr-light m-10" />
-
         <Label text="Sponsors" class="font-weight-bold m-t-10" />
         <Label class="hr-dark m-10" />
         <StackLayout orientation="horizontal" horizontalAlignment="center">
